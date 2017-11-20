25c9aea5fc3a4a021ac5840c0b55906f2382c7e2
Laraltair <wuwzhs@gmail.com>
2017-11-19 23:13:29 -0800

    start testing

---
+++ cmd_add.py
@@ -0,0 +1,3 @@
+import os
+
+os.system('sudo /home/wu/behavioral-model/targets/simple_switch/simple_switch_CLI --thrift-port=9090 < /home/wu/burst_offloading/cmd.txt')
---
+++ start_test_topo.py
@@ -0,0 +1,3 @@
+import os
+
+os.system("sudo python test_topo.py --behavioral-exe /home/wu/behavioral-model/targets/simple_switch/simple_switch --json bo_switch.json/bo_switch.json")
